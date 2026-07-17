65b6f1c8c847f12b1bff434ba00c736a936f984e
Mudabbir <Mudabbir2809@outlook.com>
2026-07-18 00:46:00 +0530

    1

---
+++ Mudabbir/Print.py
@@ -0,0 +1,18 @@
+print(''' Power and Survival
+
+Cersei Lannister: "When you play the game of thrones, you win or you die. There is no middle ground."
+Tywin Lannister: "A lion does not concern himself with the opinion of sheep."
+Petyr 'Littlefinger' Baelish: "Chaos isn't a pit. Chaos is a ladder. Many who try to climb it fail and never get to try again."
+Varys: "Power resides where men believe it resides. It’s a trick, a shadow on the wall. And a very small man can cast a very large shadow." 
+Wisdom and Identity
+
+Tyrion Lannister: "Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you."
+Jon Snow: "You know nothing, Jon Snow." (Repeated by Ygritte)
+Ned Stark: "The man who passes the sentence should swing the sword."
+Tyrion Lannister: "That's what I do: I drink and I know things." 
+Honor and Death
+
+House Stark: "Winter is coming."
+Syrio Forel: "What do we say to the Lord of Death? Not today."
+Ned Stark: "I grew up with soldiers. I learned to die a long time ago."
+Arya Stark: "Leave one wolf alive and the sheep are never safe." ''')
