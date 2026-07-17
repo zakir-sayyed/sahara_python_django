0a723d682d8032f53841ec3bb4d3230a6eb05d31
Mudabbir <Mudabbir2809@outlook.com>
2026-07-18 03:06:36 +0530

    4

---
+++ Mudabbir/Calculator.py
@@ -0,0 +1,16 @@
+python
+Python 3.14.4 (main, Jun 18 2026, 14:25:02) [GCC 15.2.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+Ctrl click to launch VS Code Native REPL
+>>> 1864 *657
+1224648
+>>> 2+5*6
+32
+>>> 86#4
+86
+>>> 71-81
+-10
+>>> 850-83
+767
+>>> 
+[4]+  Stopped                    python
