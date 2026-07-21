c9642f70f4d1e463bc022a8fcca4f49b88ce9589
Mudassir <mudassir.official2001@gmail.com>
2026-07-21 14:29:35 +0530

    added the folder jaker

---
+++ jaker/division.py
@@ -0,0 +1,10 @@
+# Division Program
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num2 == 0:
+    print("Division by zero is not allowed")
+else:
+    result = num1 / num2
+    print("Division =", result)
