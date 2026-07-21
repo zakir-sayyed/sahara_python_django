76d0d1c906a63d3ec92005ba02f67a7e4bb95d86
Rehan Shaikh <rihanshaikh744@email.com>
2026-07-21 13:07:17 +0530

    added practice

--- rehan/main_practice.py
+++ rehan/main_practice.py
@@ -1,1 +1,35 @@
- hii:
+print("========== ATM ==========")
+
+balance = 10000
+
+while True:
+    print("\n1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        print("Current Balance:", balance)
+
+    elif choice == "2":
+        amount = float(input("Enter deposit amount: "))
+        balance += amount
+        print("Money Deposited Successfully")
+
+    elif choice == "3":
+        amount = float(input("Enter withdrawal amount: "))
+
+        if amount <= balance:
+            balance -= amount
+            print("Please collect your cash")
+        else:
+            print("Insufficient Balance")
+
+    elif choice == "4":
+        print("Thank you for using ATM")
+        break
+
+    else:
+        print("Invalid Choice")
