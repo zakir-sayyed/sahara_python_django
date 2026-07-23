ae9408481c58769b8020378c3b5b4d5596c3cf14
Mudassir <mudassir.official2001@gmail.com>
2026-07-23 23:49:05 +0530

    added the poultry farm file

---
+++ poultry.py
@@ -0,0 +1,6 @@
+total_chickens = int(input("Enter total chickens: "))
+sold_chickens = int(input("Enter sold chickens: "))
+
+remaining_chickens = total_chickens - sold_chickens
+
+print("Remaining chickens:", remaining_chickens)
